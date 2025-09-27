07a384acee8046cdab11f259a4f866d02ff72559
Vishal Ambhore <vishalambhore2954@gmail.com>
2025-09-27 11:07:08 -0700

    uploading fresh frontend with login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm! : FormGroup;
+
+
+  onSubmit(){}
+}
