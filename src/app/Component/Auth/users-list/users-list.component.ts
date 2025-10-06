da3131b6c412b386a4a92c22994deeeccf8045c0
Vishal <vishal.ambhore@omunim.com>
2025-10-06 23:42:24 +0530

    users list

--- src/app/Component/Auth/users-list/users-list.component.ts
+++ src/app/Component/Auth/users-list/users-list.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignupService } from '../../../Services/Signup/signup.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  userList: any;
+  userRole: string = 'admin';
+
+  constructor(private userService: SignupService) { }
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.userService.getUsers().subscribe((users:any)=>{
+      this.userList = users;
+      console.log("User list is",this.userList)
+    })
+  }
+
+  deleteUser(id: number) {
+    if (confirm('Are you sure to delete this user?')) {
+      this.userService.deleteUser(id).subscribe((response: any) => {
+        if (response) {
+          alert('User deleted successfully');
+          this.getAllUsers();
+        } else {
+          alert('Failed to delete user');
+        }
+      });
+    }
+  }
+
+  makeAdmin(id: number) {
+    alert('Making admin is in progress, we will commited to make this feature soon.');
+  }
 
 }
